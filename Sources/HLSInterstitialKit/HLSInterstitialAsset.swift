b4867759c6cd7b5cbbee10409970035f08476f33
theRealRobG <robertnw5@gmail.com>
2021-04-11 01:01:58 +0100

    The basis of the public API

---
+++ Sources/HLSInterstitialKit/HLSInterstitialAsset.swift
@@ -0,0 +1,41 @@
+import Foundation
+import AVFoundation
+
+public final class HLSInterstitialAsset: AVURLAsset {
+    private let originalURL: URL
+    private let defaultResourceLoaderDelegateQueue = DispatchQueue(
+        label: "com.csaikit.csaiasset.default-resource-loader-delegate-queue",
+        qos: .userInitiated
+    )
+    private let resourceLoaderDelegate: HLSInterstitialAssetResourceLoaderDelegate
+    
+    public override init(url: URL, options: [String: Any]? = nil) {
+        self.originalURL = url
+        self.resourceLoaderDelegate = HLSInterstitialAssetResourceLoaderDelegate()
+        super.init(url: url.toInterstitialURL(), options: options)
+        // We set our own resourceLoader.delegate to handle HLSInterstitialScheme URL requests for manifest manipulation
+        resourceLoader.setDelegate(resourceLoaderDelegate, queue: defaultResourceLoaderDelegateQueue)
+        // We listen for changes to the resourceLoader.delegate to ensure our delegate is always set
+        resourceLoader.addObserver(
+            self, forKeyPath: #keyPath(AVAssetResourceLoader.delegate),
+            options: .new,
+            context: nil
+        )
+    }
+    
+    override public func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        guard keyPath == #keyPath(AVAssetResourceLoader.delegate) else { return }
+        guard resourceLoader.delegate === resourceLoaderDelegate else {
+            // Update the delegate that the client has set
+            resourceLoaderDelegate.clientSetResourceLoaderDelegate = resourceLoader.delegate
+            // We also update to using the queue that the client has specified
+            resourceLoader.setDelegate(resourceLoaderDelegate, queue: resourceLoader.delegateQueue)
+            return
+        }
+    }
+}
---
+++ Sources/HLSInterstitialKit/HLSInterstitialAssetResourceLoaderDelegate.swift
@@ -0,0 +1,75 @@
+import AVFoundation
+
+class HLSInterstitialAssetResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate {
+    // We keep reference to any client set AVAssetResourceLoaderDelegate to forward on events we don't handle
+    weak var clientSetResourceLoaderDelegate: AVAssetResourceLoaderDelegate?
+    
+    let playlistLoader: HLSInterstitialPlaylistLoader
+
+    init(playlistLoader: HLSInterstitialPlaylistLoader = HLSInterstitialPlaylistLoader()) {
+        self.playlistLoader = playlistLoader
+    }
+    
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForRenewalOfRequestedResource renewalRequest: AVAssetResourceRenewalRequest
+    ) -> Bool {
+        return clientSetResourceLoaderDelegate?.resourceLoader?(
+            resourceLoader,
+            shouldWaitForRenewalOfRequestedResource: renewalRequest
+        ) ?? false
+    }
+    
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest
+    ) -> Bool {
+        if let url = loadingRequest.request.url, url.isInterstitialURL() == true {
+            playlistLoader.loadPlaylist(forInterstitialURL: url) { result in
+                switch result {
+                case .success(let playlistData):
+                    loadingRequest.dataRequest?.respond(with: playlistData)
+                    loadingRequest.finishLoading()
+                case .failure(let error):
+                    loadingRequest.finishLoading(with: error)
+                }
+            }
+            return true
+        }
+        return clientSetResourceLoaderDelegate?.resourceLoader?(
+            resourceLoader,
+            shouldWaitForLoadingOfRequestedResource: loadingRequest
+        ) ?? false
+    }
+    
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForResponseTo authenticationChallenge: URLAuthenticationChallenge
+    ) -> Bool {
+        return clientSetResourceLoaderDelegate?.resourceLoader?(
+            resourceLoader,
+            shouldWaitForResponseTo: authenticationChallenge
+        ) ?? false
+    }
+    
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        didCancel authenticationChallenge: URLAuthenticationChallenge
+    ) {
+        clientSetResourceLoaderDelegate?.resourceLoader?(
+            resourceLoader,
+            didCancel: authenticationChallenge
+        )
+    }
+    
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        didCancel loadingRequest: AVAssetResourceLoadingRequest
+    ) {
+        // TODO - SHOULD HANDLE CANCELLING HERE AS WELL
+        clientSetResourceLoaderDelegate?.resourceLoader?(
+            resourceLoader,
+            didCancel: loadingRequest
+        )
+    }
+}
