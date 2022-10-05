c37e59587b2ac0b8d433c7032c98815362af469d
theRealRobG <robertnw5@gmail.com>
2022-10-05 00:09:00 -0400

    Update ad assets

--- ReferenceApp/ReferenceApp/AdvertService.swift
+++ ReferenceApp/ReferenceApp/AdvertService.swift
@@ -12,8 +12,8 @@ import HLSInterstitialKit
 class AdvertService {
     private let adDuration = 15.0
     private let urls = [
-        URL(string: "https://mssl.fwmrm.net/m/1/169843/59/6662075/YVWF0614000H_ENT_MEZZ_HULU_1925786_646/master_cmaf.m3u8")!,
-        URL(string: "https://mssl.fwmrm.net/m/1/169843/17/6662161/SBON9969000H_ENT_MEZZ_HULU_1925782_646/master_cmaf.m3u8")!
+        URL(string: "https://akam.daps.nbcuni.com/m/1/169843/19/11616659/015K831ANW12021H_ENT_MEZZ_HULU_3318934_730/master_cmaf.m3u8")!,
+        URL(string: "https://akam.daps.nbcuni.com/m/1/169843/71/11616583/015K601GON12021H_ENT_MEZZ_HULU_3318920_730/master_cmaf.m3u8")!
     ]
     private let preRollURLs = [
         URL(string: "https://akam.daps.nbcuni.com/m/1/169843/104/11614952/030G831NBC11021H_ENT_MEZZ_HULU_3318615_730/master_cmaf.m3u8")!,
